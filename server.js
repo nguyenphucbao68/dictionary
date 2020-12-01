ff113d6c918edbe5e342bee4e280b1e3193c24ba
unknown <nguyenphucbao098@gmail.com>
2020-12-02 00:06:08 +0700

    sync server

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const express = require("express");
-const next = require("next");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const express = require('express');
+const next = require('next');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const port = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV.trim() === "development";
-console.log(dev);
+const dev = process.env.NODE_ENV.trim() === 'development';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const path = require('path');
 
 const apiPaths = {
-  "/api": {
-    target: "http://[::1]:8080",
+  '/api': {
+    target: 'http://[::1]:8080',
     changeOrigin: true,
   },
 };
@@ -21,17 +20,17 @@ app
   .then(() => {
     const server = express();
 
-    server.get(["/dictionary"], (req, res) => {
-      res.sendFile(__dirname + "/public/dictionary.html");
-    })
+    server.get(['/dictionary'], (req, res) => {
+      res.sendFile(__dirname + '/public/dictionary.html');
+    });
 
-    server.get(["/"], (req, res) => {
-      res.sendFile(__dirname + "/public/index.html");
-    })
+    server.get(['/'], (req, res) => {
+      res.sendFile(__dirname + '/public/index.html');
+    });
 
-    server.use(["/api"], createProxyMiddleware(apiPaths["/api"]));
+    server.use(['/api'], createProxyMiddleware(apiPaths['/api']));
 
-    server.all("*", (req, res) => {
+    server.all('*', (req, res) => {
       return handle(req, res);
     });
     // console.log(path.join(__dirname+'/public/dictionary.html'));
@@ -45,5 +44,5 @@ app
     });
   })
   .catch((err) => {
-    console.log("Error:::::", err);
+    console.log('Error:::::', err);
   });
