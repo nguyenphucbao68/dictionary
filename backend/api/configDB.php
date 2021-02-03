8088e5c18d68ed5527ac4ee89a389036d29b5cba
unknown <nguyenphucbao098@gmail.com>
2021-02-03 17:39:31 +0700

    update config

--- backend/api/configDB.php
+++ backend/api/configDB.php
@@ -1,7 +1,7 @@
 <?php 
 // -> BD Configs <-
-define('DB_HOST', 'localhost');
-define('DB_PORT', 3306);
-define('DB_NAME', 'athoni');
-define('DB_USER', 'root');
+define('DB_HOST', '');
+define('DB_PORT', 0);
+define('DB_NAME', '');
+define('DB_USER', '');
 define('DB_PASSWORD', '');
