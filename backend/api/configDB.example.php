3337ac0e092eb91493f74f899164867bb88be139
baoc75 <chibao123hcm@gmail.com>
2021-02-18 00:09:31 +0700

    ES Backend

--- backend/api/configDB.example.php
+++ backend/api/configDB.example.php
@@ -5,3 +5,10 @@ define('DB_PORT', 0);
 define('DB_NAME', '');
 define('DB_USER', '');
 define('DB_PASSWORD', '');
+
+// ElasticSearch config
+define('ELASTIC_USERNAME', 'elastic');
+define('ELASTIC_PASSWORD', '');
+define('ELASTIC_PORT', 9200);
+define('ELASTIC_HOIDAP_INDEX', 'hoidap');
+define('ELASTIC_LECTTR_INDEX', 'lecttr');
